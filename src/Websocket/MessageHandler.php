27c470ea1b4f1fbf204c4e17806d162539310a5e
pierre.martel <pmartel502@gmail.com>
2022-08-26 15:26:43 +0200

    messagerie avec websocket

---
+++ src/Websocket/MessageHandler.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Websocket;
+ 
+use Exception;
+use SplObjectStorage;
+use Ratchet\ConnectionInterface;
+use Ratchet\MessageComponentInterface;
+ 
+ 
+class MessageHandler implements MessageComponentInterface
+{
+ 
+    protected $connections;
+ 
+    public function __construct()
+    {
+        $this->connections = new SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->connections->attach($conn);
+ 
+    }
+ 
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach($this->connections as $connection)
+        {
+            if($connection === $from)
+            {
+                continue;
+            }
+            $connection->send($msg);
+        }
+ 
+    }
+ 
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->connections->detach($conn);
+    }
+ 
+    public function onError(ConnectionInterface $conn, Exception $e)
+    {   
+        $this->connections->detach($conn);
+        $conn->close();
+ 
+    }
+}
